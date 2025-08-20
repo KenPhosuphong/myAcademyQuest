3dca758d0524a97b45573953d2bf026f78f103b2
MacYuem Ken <oddsmacken@MacYuems-MacBook-Air.local>
2025-08-20 17:59:02 +0700

    add more expect case

--- spec/e2e/quest_e2e_spec.rb
+++ spec/e2e/quest_e2e_spec.rb
@@ -4,5 +4,6 @@ RSpec.describe "Quests", type: :system do
   it 'creates a new quest' do
     visit root_path
     expect(page).to have_current_path(root_path)
+    expect(page).to have_content('My Academy Quests')
   end
 end
