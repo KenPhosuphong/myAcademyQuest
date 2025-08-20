b82acb0573ebf804ba2ba24886a34055e95a845b
MacYuem Ken <oddsmacken@MacYuems-MacBook-Air.local>
2025-08-20 16:27:17 +0700

    test: remove test for path that remove

--- spec/requests/quests_spec.rb
+++ spec/requests/quests_spec.rb
@@ -40,21 +40,6 @@ RSpec.describe "/quests", type: :request do
     end
   end
 
-  describe "GET /new" do
-    it "renders a successful response" do
-      get new_quest_url
-      expect(response).to be_successful
-    end
-  end
-
-  describe "GET /edit" do
-    it "renders a successful response" do
-      quest = Quest.create! valid_attributes
-      get edit_quest_url(quest)
-      expect(response).to be_successful
-    end
-  end
-
   describe "POST /create" do
     context "with valid parameters" do
       it "creates a new Quest" do
--- spec/routing/quests_routing_spec.rb
+++ spec/routing/quests_routing_spec.rb
@@ -6,25 +6,8 @@ RSpec.describe QuestsController, type: :routing do
       expect(get: "/quests").to route_to("quests#index")
     end
 
-    it "routes to #new" do
-      expect(get: "/quests/new").to route_to("quests#new")
-    end
-
-    it "routes to #show" do
-      expect(get: "/quests/1").to route_to("quests#show", id: "1")
-    end
-
-    it "routes to #edit" do
-      expect(get: "/quests/1/edit").to route_to("quests#edit", id: "1")
-    end
-
-
     it "routes to #create" do
       expect(post: "/quests").to route_to("quests#create")
-    end
-
-    it "routes to #update via PUT" do
-      expect(put: "/quests/1").to route_to("quests#update", id: "1")
     end
 
     it "routes to #update via PATCH" do
